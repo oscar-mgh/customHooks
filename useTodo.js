68b13772014958469582ce451ca5e316dbb2e6e8
Oscar MG <oscar.galindo1@yahoo.com>
2022-06-28 20:24:29 -0500

    frist commit

---
+++ useTodo.js
@@ -0,0 +1,49 @@
+import { useEffect, useReducer } from 'react'
+import { todoReducer } from '../08-useReducer/todoReducer'
+
+export const useTodo = () => {
+
+  const init  = () => {
+    return JSON.parse( localStorage.getItem('todos')) || [];
+  }
+  
+  const [ todos, dispatch ] = useReducer( todoReducer, [], init )
+  const todosCount = todos.length;
+  const pendingTodosCount = todos.filter( todo => !todo.done).length;
+
+  useEffect(() => {
+    localStorage.setItem( 'todos', JSON.stringify( todos ) )
+  }, [todos])
+
+  const handleNewTodo = ( todo ) => {
+    const action = {
+      type: '[TODO] Add Todo',
+      payload: todo,
+    }
+
+    dispatch( action )
+  }
+
+  const handleDeleteTodo = ( id ) => {
+    dispatch({
+      type: '[TODO] Remove Todo',
+      payload: id,
+    })
+  }
+
+  const handleToggleTodo = ( id ) => {    
+    dispatch({
+      type: '[TODO] Toggle Todo',
+      payload: id,
+    })
+  }
+
+  return {
+    todos,
+    todosCount,
+    pendingTodosCount,
+    handleNewTodo,
+    handleDeleteTodo,
+    handleToggleTodo,
+  }
+}
